ffc2e0122e7a6e0398acd5092ef4efc5dc245166
MD SULAIMAN <smensulaiman007@gmail.com>
2025-06-25 18:11:48 +0900

    toggle icon added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,29 @@
-import {Cloud} from "lucide-react";
+import {Cloud, Moon, Sun} from "lucide-react";
 import {Link} from "react-router-dom";
 import {useTheme} from "@/components/context/theme-provider.tsx";
 
 const Header = () => {
 
-    const { theme, setTheme } = useTheme();
+    const {theme, setTheme} = useTheme();
     const isDark = theme === "dark";
 
     return (
-        <header className="sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur p-2 supports-[backdrop-filter]:bg-background/40">
-            <div className="container mx-auto flex">
+        <header
+            className="sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur py-4 supports-[backdrop-filter]:bg-background/40">
+            <div className="container mx-auto flex h-16 items-center justify-between px-4">
                 <Link to={"/"}>
-                    <Cloud className={ isDark ? "text-green-400" : "text-black" } width="40" height="40"/>
-                    <h3 className={ (isDark ? "text-green-400" : "text-black") + " text-xl" }>Abhawa</h3>
+                    <Cloud className={isDark ? "text-green-400" : "text-black"} width="40" height="40"/>
+                    <h3 className={(isDark ? "text-green-400" : "text-black") + " text-xl"}>Abhawa</h3>
                 </Link>
-            </div>
-            <div>
-                {/*search*/}
-                {/*theme toggle*/}
-                <div className="cursor-pointer" onClick={ function (): void { setTheme(isDark ? "light" :  "dark") } }>toggle</div>
+                <div>
+                    {/*search*/}
+                    {/*theme toggle*/}
+                    <div className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? "rotate-180" : "rotate-0"}`}
+                         onClick={function (): void { setTheme(isDark ? "light" : "dark")}}>
+                        {(isDark ? (<Sun className="h-6 w-6 text-yellow-400 rotate-0 transition-all"/>)
+                            : (<Moon className="h-6 w-6 text-black rotate-0 transition-all"/>))}
+                    </div>
+                </div>
             </div>
         </header>
     );
